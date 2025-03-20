6e2796d832b44a305b918450e8f326c2efee5fa2
wellington moreira <wsantos08@hotmail.com>
2025-03-20 20:54:03 -0300

    ignorando mensagens de grupos ou status

--- app.py
+++ app.py
@@ -15,22 +15,21 @@ app = Flask(__name__)
 @app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
     data = request.json
+    chat_id = data['payload']['from']
+    received_message = data['payload']['body']
+    is_group = '@g.us' in chat_id
+    is_status = 'status@broadcast'in chat_id
 
-    print(f'EVENTO RECEBIDO: {data}')
+    if is_group or is_status:
+        return jsonify({'status': 'success', 'message': 'Mensagem de grupo/status ignorada.'}), 200
 
     waha = Waha()
     ai_bot = AIBot()
 
-    chat_id = data['payload']['from']
-    received_message = data['payload']['body']
-
     waha.start_typing(chat_id=chat_id)
-    time.sleep(random.randint(3, 10))
-
     response = ai_bot.invoke(question=received_message)
     waha.send_message(
         chat_id=chat_id,
-        # message='To away, esta resposta é automática gerada com python, respondo depois...',
         message=response,
     )
     waha.stop_typing(chat_id=chat_id)
